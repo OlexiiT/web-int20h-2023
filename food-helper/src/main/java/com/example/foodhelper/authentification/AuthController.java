49aed574386a19c3d4a19329d7a7748dc2361562
Olexii <duzevyhovocet242@gmail.com>
2023-02-06 00:45:23 +0200

    package changed

--- food-helper/src/main/java/com/example/foodhelper/authentification/AuthController.java
+++ food-helper/src/main/java/com/example/foodhelper/authentification/AuthController.java
@@ -1,4 +1,4 @@
-package com.example.foodhelper.controllers;
+package com.example.foodhelper.authentification;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
